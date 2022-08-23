dedd0db8e1b9a7e26c4c0e6a60655f7d01a7aa30
Augusto Ventura <paivaav@gmail.com>
2022-08-23 20:39:02 -0300

    Render ul items

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 //React 17
-ReactDOM.render(<p>My first page powered by react</p>, document.getElementById('root'))
+ReactDOM.render(
+  <p>My first page powered by react</p>,
+  document.getElementById("root")
+);
+ReactDOM.render(
+  <ul>
+    <li>Hi</li>
+    <li>Are you there</li>
+  </ul>,
+  document.getElementById("root")
+);
 
 // React 18
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 
 const root = ReactDOM.createRoot(container);
 
-root.render(<h1>Hello, everyone!</h1>)
+root.render(<h1>Hello, everyone!</h1>);
 
-root.render(<h1>I'm am Augusto and I'm here!</h1>)
+root.render(<h1>I'm am Augusto and I'm here!</h1>);
 
+root.render(
+  <ul>
+    <li>Hi</li>
+    <li>Are you there</li>
+  </ul>
+);
