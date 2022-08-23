3c7662629fce15a34c7138f27871738794ffb126
Augusto Ventura <paivaav@gmail.com>
2022-08-23 20:28:16 -0300

    Firs react app using version 17 and 18

---
+++ index.js
@@ -0,0 +1,12 @@
+//React 17
+ReactDOM.render(<p>My first page powered by react</p>, document.getElementById('root'))
+
+// React 18
+const container = document.getElementById('root');
+
+const root = ReactDOM.createRoot(container);
+
+root.render(<h1>Hello, everyone!</h1>)
+
+root.render(<h1>I'm am Augusto and I'm here!</h1>)
+
